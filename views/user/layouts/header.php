e080b9c1a355ebbecca1067dd09317e1d60221da
manikbayu23 <gdmanikmas@gmail.com>
2024-12-06 08:14:19 +0800

    add user assets

--- views/user/layouts/header.php
+++ views/user/layouts/header.php
@@ -1,3 +1,33 @@
-<?php
-echo 'ini header';
-include __DIR__ . '/../components/navbar.php';
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>KOPPEE - Coffee Shop HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free Website Template" name="keywords">
+    <meta content="Free Website Template" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="/pondok-gita/assets/user/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="/pondok-gita/assets/user/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="/pondok-gita/assets/user/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php
+    include __DIR__ . '/../components/navbar.php';
